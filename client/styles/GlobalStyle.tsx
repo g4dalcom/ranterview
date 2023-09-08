0eb51cd5b9c51188a18841d0fb629fcea31a2165
g4dalcom <g4dalcom@gmail.com>
2023-09-08 22:46:43 +0900

    fix: reset css 및 global style 적용

--- client/styles/GlobalStyle.tsx
+++ client/styles/GlobalStyle.tsx
@@ -1,72 +1,17 @@
 import { css, Global } from '@emotion/react';
+import resetStyle from './reset';
 
 const style = css`
-  blockquote,
-  body,
-  dd,
-  dl,
-  dt,
-  fieldset,
-  figure,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  hr,
-  html,
-  iframe,
-  legend,
-  li,
-  ol,
-  p,
-  pre,
-  textarea,
-  ul {
-    margin: 0;
-    padding: 0;
-  }
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-size: 100%;
-    font-weight: 400;
-  }
-  ul {
-    list-style: none;
-  }
-  button,
-  input,
-  select {
-    margin: 0;
-  }
+  ${resetStyle}
+
   html {
-    box-sizing: border-box;
+    font-size: 62.5%;
   }
-  *,
-  :after,
-  :before {
-    box-sizing: inherit;
-  }
-  img,
-  video {
-    height: auto;
-    max-width: 100%;
-  }
-  iframe {
-    border: 0;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-  td,
-  th {
-    padding: 0;
+
+  body {
+    font-size: 1.6rem;
+    font-family: 'Pretendard';
+    font-weight: 300;
   }
 `;
 
