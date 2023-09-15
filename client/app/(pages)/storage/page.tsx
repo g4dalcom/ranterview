7bb448c7d367f25551876c7cd8b1ab21e9420ff1
g4dalcom <g4dalcom@gmail.com>
2023-09-15 12:17:03 +0900

    feat: mui 관련 라이브러리 설치 및 storage 페이지 테이블 구조 작업

--- client/app/(pages)/storage/page.tsx
+++ client/app/(pages)/storage/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import DataTable from '@/app/components/dataTable/dataTable';
+
 const Storage = () => {
   return (
     <div>
-      <h1>Storage page</h1>
+      <h1>Storage Page</h1>
+      <DataTable />
     </div>
   );
 };
---
+++ client/app/components/dataTable/style.ts
@@ -0,0 +1,12 @@
+import styeld from '@emotion/styled';
+
+export const DataTable = styeld.div`
+
+    & .data_grid {
+        padding: 20px;
+    }
+ 
+    & .title_row {
+        font-size: 1.6rem;
+    }
+`;
